6a7512bb7c6e4946f0a7f488514750dc7a86e1da
Rafael ESPARON <rafael.esparon2023@campus-eni.fr>
2024-02-19 10:30:46 +0100

    bootstrap added

--- importmap.php
+++ importmap.php
@@ -27,4 +27,18 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.2',
+        'type' => 'css',
+    ],
+    'bootstrap/dist/js/bootstrap.min.js' => [
+        'version' => '5.3.2',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap-icons/font/bootstrap-icons.min.css' => [
+        'version' => '1.11.3',
+        'type' => 'css',
+    ],
 ];
