6ac49a271d23b11ad794c0e5320481f16713a17e
kdrouet2023 <kevin.drouet.44@gmail.com>
2024-02-20 09:22:08 +0100

    ProfilType

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +12,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
+        return $this->render('main/index.html.twig',[
             'controller_name' => 'MainController',
         ]);
     }
