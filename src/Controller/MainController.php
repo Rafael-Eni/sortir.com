c9a6b4bf9f4b8d2e1d0266b76922bb2234b44b21
Laura Bensimon <123237987+HiiSplot@users.noreply.github.com>
2024-02-23 13:54:26 +0100

    Modif css

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,4 +16,22 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+
+    #[Route('/politique-de-confidentialité', name: 'app_politique')]
+    public function politiqueDeConfidentialité(): Response
+    {
+        return $this->render('footer/politique.html.twig',[
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions')]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('footer/mentions.html.twig',[
+            'controller_name' => 'MainController',
+        ]);
+    }
+
 }
