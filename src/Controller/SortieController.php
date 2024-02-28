06c040b726ab149ed2bb4eefa367a95395c25fac
resparon2023 <rafael.esparon2023@campus-eni.fr>
2024-02-28 11:00:53 +0100

    maps api implemented

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/sortie')]
 #[IsGranted('ROLE_USER')]
@@ -58,7 +59,7 @@ class SortieController extends AbstractController
     }
 
     #[Route('/new', name: 'app_sortie_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository, HttpClientInterface $httpClient): Response
     {
         $sortie = new Sortie();
         $form = $this->createForm(SortieType::class, $sortie);
@@ -71,32 +72,64 @@ class SortieController extends AbstractController
         }
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Create Ville
-            $ville = $sortie->getLieu()->getVille();
-            $entityManager->persist($ville);
 
-            // Create lieu
-            $lieu = $sortie->getLieu();
-            $entityManager->persist($lieu);
+            $street = $sortie->getLieu()->getRue();
+            $zipCode = $sortie->getLieu()->getVille()->getCodePostal();
+            // Fetching lat lon
+            $result = $httpClient->request('GET', 'https://api-adresse.data.gouv.fr/search/', [
+                'query' => [
+                    'q' => $street,
+                    'postcode' =>$zipCode,
+                ],
+            ]);
 
-            // Set oraganisateur
-            $sortie->setOrganisateur($this->getUser());
+            $data = json_decode($result->getContent(), true);
 
-            // Set creation state
-            $isPublished = $sortie->isIsPublished();
-            $state = $isPublished ? 2 : 1;
-            $creationState = $etatRepository->find($state);
-            $sortie->setEtat($creationState);
+            $lat = $request->get('lat');
+            $lon = $request->get('lon');
 
-            // Persist entity
-            $entityManager->persist($sortie);
-            $entityManager->flush();
+            if ($lat && $lon) {
+                // Create Ville
+                $ville = $sortie->getLieu()->getVille();
+                $entityManager->persist($ville);
 
-            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+                // Create lieu
+                $lieu = $sortie->getLieu();
+                $entityManager->persist($lieu);
+
+                // Set oraganisateur
+                $sortie->setOrganisateur($this->getUser());
+
+                // Set creation state
+                $isPublished = $sortie->isIsPublished();
+                $state = $isPublished ? 2 : 1;
+                $creationState = $etatRepository->find($state);
+                $sortie->setEtat($creationState);
+
+                // Set coordinates
+                $sortie->getLieu()->setLongitude($lon);
+                $sortie->getLieu()->setLatitude($lat);
+
+                // Persist entity
+                $entityManager->persist($sortie);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('sortie/new.html.twig', [
+                'sortie' => $sortie,
+                'form' => $form,
+                'api' => $data
+            ]);
+
+
+
         }
         return $this->render('sortie/new.html.twig', [
             'sortie' => $sortie,
             'form' => $form,
+            'api' => null,
         ]);
     }
 
